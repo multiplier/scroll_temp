8fd70bfc9b5bbdc718ef161a1b0aad7ef839b031
John Kamuchau <webflamesolutions@gmail.com>
2013-05-30 16:16:02 +0930

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScrollTemp::Application.config.secret_key_base = '979128ff4037cd41c3abae555ae86e39546b4812d25d7cad3d42f3b8e5ac295c3b5ba564d22d01be72f85beffd2a84dbbf9b1b036e963dd3ae032bc047fd17cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ScrollTemp::Application.config.session_store :cookie_store, key: '_scroll_temp_session'
